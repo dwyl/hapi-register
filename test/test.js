ca50ccd61b469b5790acb2441a735853ffb8f452
Nelson <contact.nelsonic@gmail.com>
2015-09-20 07:16:42 +0100

    first test passes - now the fun part starts!

--- test/test.js
+++ test/test.js
@@ -1,11 +1,22 @@
-var Hapi   = require('hapi');     // https://github.com/nelsonic/learn-hapi
-var server = new Hapi.Server({ debug: false })
-server.connection();
+var test   = require('tape');
+var server = require('./server.test.js');
 
-// load one plugin
-server.register(require('../lib'), function (err) {
-    if (err) {
-        console.error('Failed to load plugin:', err);
-    }
+var email  = 'dwyl.test+auth_basic' +Math.random()+'@gmail.com';
 
+var person = {
+  "email"    : email,
+  "password" : "PinkFluffyUnicorns"
+}
+
+test("request to GET / should not require session token", function(t) {
+  var options = {
+    method: "POST",
+    url: "/register",
+    payload : person
+  };
+
+  server.inject(options, function(response) {
+    t.equal(response.statusCode, 200, "Base URL Does not Require ");
+    server.stop(function(){ t.end() });
+  });
 });
