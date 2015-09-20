c4addee794a15ce0ba97f4172672cee949b7bc17
Nelson <contact.nelsonic@gmail.com>
2015-09-20 08:21:45 +0100

    adds devDep on Joi for field definition & validation

--- test/server.test.js
+++ test/server.test.js
@@ -1,10 +1,16 @@
 var Hapi   = require('hapi');     // https://github.com/nelsonic/learn-hapi
 var server = new Hapi.Server({ debug: false })
 server.connection({ port: 8000 });
-var options = {'test':'that'}
-// load one plugin
 
-server.register({ register: require('../lib'), options:options }, function (err) {
+// define which fields we want to validate for
+var Joi    = require('joi');
+var fields = {
+
+};
+var opts   = {'test':'that'};
+
+// load the plugin
+server.register([{ register: require('../lib'), options:opts }], function (err) {
   if (err) {
     console.error('Failed to load plugin:', err);
   }
