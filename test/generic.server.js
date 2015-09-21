de990cc6d70f3faeddc18f0f4f813e4cee413835
Nelson <contact.nelsonic@gmail.com>
2015-09-21 19:51:51 +0100

    removes generic server as it cannot be instantiated multiple times in tests...

--- test/generic.server.js
+++
@@ -1,4 +0,0 @@
-var Hapi   = require('hapi');     // https://github.com/nelsonic/learn-hapi
-var server = new Hapi.Server({ debug: false })
-server.connection({ port: 8000 });
-module.exports = server;
